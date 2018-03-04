e7dc99e867567d74644c933cda116e502e2422ec
Víctor Navarro González <victor.navarro@enti.cat>
2018-03-04 13:03:07 +0100

    Jump, Raycast and Movement
    All of these are done with the exception of the movement than will be improved

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour {
 
+    public static float MainHorizontal() {
+        float r = 0.0f;
+        r += Input.GetAxis("J_Horizontal");
+        r += Input.GetAxis("K_Horizontal");
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+    }
+
+    public static float MainVertical() {
+        float r = 0.0f;
+        r += Input.GetAxis("J_Vertical");
+        r += Input.GetAxis("K_Vertical");
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+    }
+
+    public static Vector2 MainJoystick() {
+        return new Vector2(MainHorizontal(), MainVertical());
+    }
+
     public static bool ButtonA() {
         return Input.GetButtonDown("ButtonA");
     }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float sideSpeed = 5.0f;
+
+    private Rigidbody2D rb;
+
+    void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	void FixedUpdate () {
+        rb.velocity = new Vector2(InputManager.MainHorizontal() * sideSpeed * Time.deltaTime, rb.velocity.y);
+    }
+}
