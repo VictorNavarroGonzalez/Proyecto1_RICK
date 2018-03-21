ac38279b3625ec35fc1e6fd44eb2d804a51f8557
Víctor Navarro González <victor.navarro@enti.cat>
2018-03-21 13:26:51 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform player;
+    public new Transform camera;
+
+    public AnimationCurve curve;
+
+    public Rigidbody2D rb;
+
+    void Awake() {
+        transform.position = player.position;
+    }
+
+    void FixedUpdate() {
+        transform.DOLocalMoveX(player.position.x, 0f);
+        
+
+
+        if (rb.velocity.y < 0f)
+        {
+            transform.DOLocalMoveY(player.position.y, 1f).SetEase(curve);
+            //StartCoroutine(CameraJump());
+            
+        }
+    }
+
+    IEnumerator CameraJump()
+    {
+        
+        
+        yield return null;
+    }
+
+}
