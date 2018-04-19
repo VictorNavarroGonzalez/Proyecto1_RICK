1a45a05d57eb7f3a6d53ce3d3e87b595ea5f073b
Víctor Navarro González <victor.navarro@enti.cat>
2018-04-19 23:18:01 +0200

    ykse bro

--- Assets/Scripts/SceneCollider.cs
+++ Assets/Scripts/SceneCollider.cs
@@ -7,10 +7,12 @@ public class SceneCollider : MonoBehaviour {
 
     public Object next;
     public GameObject player;
+    new public Camera camera;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject == player) {
             Debug.Log("Next Scene: " + next.name);
+            camera.GetComponent<CameraMovement>().Pause();
             SceneManager.LoadScene(next.name, LoadSceneMode.Single);
         }
     }
