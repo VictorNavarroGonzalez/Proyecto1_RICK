d0fef5da0262868da38dd25a9ef2033d049ce03f
Unknown <marc.solis@enti.cat>
2018-04-19 15:43:13 +0200

    respawn (must fix)

--- Assets/Scripts/SpawnCollider.cs
+++ Assets/Scripts/SpawnCollider.cs
@@ -8,11 +8,22 @@ public class SpawnCollider : MonoBehaviour {
     public GameObject spawn;
     new public Camera camera;
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.collider.gameObject == player) {
-            float d = Vector2.Distance(player.transform.position, camera.transform.position);
-            camera.transform.position = spawn.transform.position;
-            player.transform.position = camera.transform.position - new Vector3(0, d, 0);
+    //void OnCollisionEnter2D(Collision2D collision) {
+    //    if(collision.collider.gameObject == player) {
+    //        float d = Vector2.Distance(player.transform.position, camera.transform.position);
+    //        camera.transform.position = spawn.transform.position;
+    //        player.transform.position = camera.transform.position - new Vector3(0, d, 0);
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == player)
+        {
+            float distCP = Vector2.Distance(player.transform.position, camera.transform.position);
+            Vector3 pos = (player.transform.position - this.transform.position);
+            player.transform.position = spawn.transform.position + pos;
+            camera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, camera.transform.position.z);
         }
     }
 
