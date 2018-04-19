d7bb707ec665c02459cce168388395fcc4da843f
Víctor Navarro González <victor.navarro@enti.cat>
2018-04-19 21:00:02 +0200

    Scene Collider

---
+++ Assets/Scripts/SceneCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCollider : MonoBehaviour {
+
+    public Object next;
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject == player) {
+            Debug.Log("Next Scene: " + next.name);
+            SceneManager.LoadScene(next.name, LoadSceneMode.Single);
+        }
+    }
+
+}
