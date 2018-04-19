817f6409b8b86dc98547e247310889472291e71c
Unknown <carles.verdu@enti.cat>
2018-04-19 23:16:31 +0200

    Dash arreglado

--- Assets/Scripts/PlayerClasses/PlayerDash.cs
+++ Assets/Scripts/PlayerClasses/PlayerDash.cs
@@ -7,7 +7,7 @@ public class PlayerDash : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool canDash;
-
+    private Vector2 temp;
     private float _dashForce = 1000f;
     public float DashForce {
         get { return _dashForce; }
@@ -21,6 +21,8 @@ public class PlayerDash : MonoBehaviour
     private void FixedUpdate()
     {
         if (canDash == false && PlayerState.State == PlayerState.MyState.Grounding) canDash = true;
+
+
     }
 
     public bool CheckDash() {
@@ -30,10 +32,19 @@ public class PlayerDash : MonoBehaviour
 
     public void Dash() {
         canDash = false;
+        temp = rb.velocity;
         StartCoroutine(GetComponent<PlayerState>().Stopping(0.1f));
+        StartCoroutine(Counterdash());
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.AddForce(rb.velocity* DashForce* Time.deltaTime , ForceMode2D.Impulse);
     }
-
-   
+    
+   public IEnumerator Counterdash()
+    {
+        for (int i = 50; i > 1; i-=10)
+        {
+            rb.AddForce(-rb.velocity * DashForce / i * Time.deltaTime, ForceMode2D.Impulse);
+            yield return new WaitForEndOfFrame();
+        }
+    }
 }
