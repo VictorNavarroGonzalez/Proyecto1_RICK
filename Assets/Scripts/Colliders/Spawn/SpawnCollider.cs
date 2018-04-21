ab4f26512f94e7873ece2fb52780b11c918c51e9
Víctor Navarro González <victor.navarro@enti.cat>
2018-04-21 10:26:49 +0200

    Quote Renderer

---
+++ Assets/Scripts/Colliders/Spawn/SpawnCollider.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SpawnCollider : MonoBehaviour {
+
+    public GameObject spawn;
+    private GameObject player;
+    new private GameObject camera;
+
+    public bool discrete;
+    public bool delayed;
+    public float time;
+
+    void Start() {
+        player = GameObject.Find("Player");
+        camera = GameObject.Find("Camera");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject == player) {
+
+            if (!delayed) time = 0f;
+
+            if (discrete) StartCoroutine(Teleport());
+            else StartCoroutine(Respawn());
+
+        }
+    }
+
+    private IEnumerator Respawn() {
+        yield return new WaitForSeconds(time);
+        player.transform.position = spawn.transform.position;
+
+        camera.GetComponent<CameraMovement>().Pause();
+        Vector3 offset = new Vector3(0, 0, 2);
+        camera.transform.position = player.transform.position - offset;
+    }
+
+    private IEnumerator Teleport() {
+        yield return new WaitForSeconds(time);
+        float distY = player.GetComponent<PlayerBounce>().DistGround();
+        Debug.Log(player.GetComponent<PlayerBounce>().DistGround());
+        player.transform.position = spawn.transform.position + new Vector3(0,Mathf.Abs(distY) + 0.6436337f , 0);
+        camera.GetComponent<CameraMovement>().Pause();
+
+        Vector3 offset = camera.GetComponent<CameraMovement>().offset;
+        camera.transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/Colliders/Text/TextSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSpawn : MonoBehaviour {
+
+    private GameObject player;
+    public GameObject prompter;
+    public Sprite text;
+
+    void Start() {
+        player = GameObject.Find("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject == player) {
+
+            prompter.GetComponent<SpriteRenderer>().sprite = text;
+
+        }
+    }
+}
