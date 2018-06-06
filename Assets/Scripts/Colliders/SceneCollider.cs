2791d296df343f20b5089e46b96bbef8c671d4bb
Víctor Navarro González <victor.navarro@enti.cat>
2018-06-06 17:40:34 +0200

    Second Release

--- Assets/Scripts/Colliders/SceneCollider.cs
+++ Assets/Scripts/Colliders/SceneCollider.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneCollider : MonoBehaviour {
 
-    public Object nextScene;
+    public enum Scene { CIRCLE, SQUARE, BOTH };
+    public Scene scene;
+
     private GameObject player;
     new private GameObject camera;
 
@@ -21,7 +23,23 @@ public class SceneCollider : MonoBehaviour {
 
         if (collision.gameObject.tag == "Player") {
             camera.GetComponent<CameraBehaviour>().Pause();
-            SceneManager.LoadScene(nextScene.name, LoadSceneMode.Single);
+
+            switch (scene) {
+                case Scene.CIRCLE:
+                    SceneManager.LoadScene(1, LoadSceneMode.Single);
+                    DefaultValues.Levels.Current = DefaultValues.Levels.C01;
+                    break;
+
+                case Scene.SQUARE:
+                    SceneManager.LoadScene(2, LoadSceneMode.Single);
+                    DefaultValues.Levels.Current = DefaultValues.Levels.S01;
+                    break;
+
+                case Scene.BOTH:
+                    SceneManager.LoadScene(3, LoadSceneMode.Single);
+                    DefaultValues.Levels.Current = DefaultValues.Levels.B01;
+                    break;
+            }
         }
     }
 
